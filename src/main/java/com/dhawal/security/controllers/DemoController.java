5aa815dfc28a9720a6e369209a62b7a9fab422ed
Dhawal Patil <dhawalpatil200@gmail.com>
2024-08-18 18:18:35 +0530

    migrate to Dao Auth provider

--- src/main/java/com/dhawal/security/controllers/DemoController.java
+++ src/main/java/com/dhawal/security/controllers/DemoController.java
@@ -1,14 +1,28 @@
 package com.dhawal.security.controllers;
 
+import com.dhawal.security.models.User;
+import com.dhawal.security.repository.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class DemoController {
 
+    @Autowired
+    private UserRepository userRepository;
+
+
     @GetMapping("/")
     public String greet(HttpServletRequest request) {
         return "Hello world " + request.getSession().getId();
+    }
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
     }
 }
